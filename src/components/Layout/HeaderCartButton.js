6a33491b189c4dd67162c9bdaaeb5c995ed37e19
Aelbish Shrestha <aelbish@gmail.com>
2021-07-23 18:01:00 -0500

    Added React context for the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,24 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  //useContext will reevaluate this component whenever the context changes
+  //to use the context, we do not need to import provider
+  const cartCtx = useContext(CartContext);
+  //we are now accessing the item of the cart-context through the provider
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>2</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//I like to think React Context like a Schema
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default CartContext;
